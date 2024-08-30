82221e9fff37840e2d2c7e601a21e4dacab4ad8c
gp352 <gandhip352@gmail.com>
2024-08-30 23:18:58 +0530

    Added Test Cases for borrowing books

--- test_library.py
+++ test_library.py
@@ -17,3 +17,25 @@ def test_add_book_duplicate_isbn():
         library.add_book("1234567890", "Second Book", "Author B", 2024)
     assert str(excinfo.value) == "Book with this ISBN already exists"
 
+def test_borrow_book_success():
+    library = Library()
+    library.add_book("987654321", "Book Name", "Author", 2022)
+    library.borrow_book("987654321")
+    book = library.get_book("987654321")
+    assert book is not None
+    assert book.get('borrowed') is True
+
+def test_borrow_book_not_available():
+    library = Library()
+    library.add_book("987654321", "Book Name", "Author", 2022)
+    library.borrow_book("987654321")
+    with pytest.raises(ValueError) as excinfo:
+        library.borrow_book("987654321")
+    assert str(excinfo.value) == "Book borrowed already"
+
+def test_borrow_nonexistent_book():
+    library = Library()
+    with pytest.raises(KeyError) as excinfo:
+        library.borrow_book("nonexistent_isbn")
+    assert excinfo.value.args[0] == "Book is not there"
+
